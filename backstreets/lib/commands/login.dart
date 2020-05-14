c942c82d69ccd5518a82fb2c930f2d1ddd55e2f9
Chris Norman <chris.norman2@googlemail.com>
2020-05-14 12:20:30 +0100

    Added sounds for invalid logins.

--- backstreets/lib/commands/login.dart
+++ backstreets/lib/commands/login.dart
@@ -2,20 +2,29 @@
 library login;
 
 import '../game/account.dart';
+import '../sound.dart';
 
 import 'command.dart';
 import 'command_context.dart';
+
+/// The sound to play when the player logs in.
+const String loginSound = 'general/welcome.wav';
+
+/// The sound to play when there is a login failure.
+const String loginError = 'general/loginerror.wav';
 
 final Command createAccount = Command(
   'createAccount', (CommandContext ctx) {
     final String username = ctx.args[0] as String;
     final String password = ctx.args[1] as String;
     if (accounts.containsKey(username)) {
-      return ctx.sendError('That username is already taken.');
+      return ctx.sendError('That username is already taken.', sound: Sound(loginError));
     }
     ctx.account = Account(username);
+    accounts[ctx.account.username] = ctx.account;
     ctx.account.setPassword(password);
     ctx.sendAccount(ctx.account);
+    ctx.sendInterfaceSound(Sound(loginSound));
   }, authenticationType: AuthenticationTypes.anonymous
 );
 
@@ -26,8 +35,10 @@ final Command login = Command(
     if (accounts.containsKey(username) && accounts[username].verify(password)) {
       final Account account = accounts[username];
       ctx.account = account;
-      return ctx.sendAccount(account);
+      ctx.sendAccount(account);
+      ctx.sendInterfaceSound(Sound(loginSound));
+    } else {
+      ctx.sendError('Invalid username or password.', sound: Sound(loginError));
     }
-    ctx.sendError('Invalid username or password.');
   }, authenticationType: AuthenticationTypes.anonymous
 );
