04a44532c6af88f085ce1b4708ab59a3cf1a0645
Chris Norman <chris.norman2@googlemail.com>
2020-05-21 20:24:50 +0100

    Show a message when a map is renamed.

--- backstreets/lib/commands/building.dart
+++ backstreets/lib/commands/building.dart
@@ -13,5 +13,6 @@ final Command renameMap = Command('renameMap', (CommandContext ctx) async {
     ..values.name = ctx.args[0] as String
     ..where((GameMap m) => m.id).equalTo(ctx.mapId);
   final GameMap m = await q.updateOne();
+  ctx.sendMessage('Map renamed.');
   ctx.send('mapName', <String>[m.name]);
 }, authenticationType: AuthenticationTypes.admin);
