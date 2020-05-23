3b6a40bec7eb2f7fa83678aae4198cf45882e6d8
Chris Norman <chris.norman2@googlemail.com>
2020-05-24 00:35:26 +0100

    Run with only 1 isolate.

--- backstreets/bin/main.dart
+++ backstreets/bin/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+// import 'dart:io';
 
 import 'package:aqueduct/aqueduct.dart';
 import 'package:backstreets/backstreets.dart';
@@ -8,8 +8,9 @@ Future<void> main() async {
       ..options.configurationFilePath = 'config.yaml'
       ..options.port = 8888;
 
-  final int count = Platform.numberOfProcessors ~/ 2;
-  await app.start(numberOfInstances: count > 0 ? count : 1);
+  // final int count = Platform.numberOfProcessors ~/ 2;
+  // await app.start(numberOfInstances: count > 0 ? count : 1);
+  await app.start(numberOfInstances: 1);
 
   print('Application started on port: ${app.options.port}.');
   print('Use Ctrl-C (SIGINT) to stop running the application.');
