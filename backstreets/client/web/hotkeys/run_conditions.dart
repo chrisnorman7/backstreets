f00518d086e3468af39d6490290e63c5ac0b4059
Chris Norman <chris.norman2@googlemail.com>
2020-05-23 12:50:31 +0100

    Don't allow admin commands when a menu is showing.

--- backstreets/client/web/hotkeys/run_conditions.dart
+++ backstreets/client/web/hotkeys/run_conditions.dart
@@ -4,7 +4,7 @@ library run_conditions;
 import '../main.dart';
 
 /// Returns true if the player is connected to an admin character.
-bool admin() => commandContext.admin == true;
+bool admin() => commandContext != null && commandContext.book == null && commandContext.admin == true;
 
 /// Returns true if the map has been loaded, and there is no book in the way.
 bool validMap() => commandContext != null && commandContext.mapName != null && commandContext.book == null;
