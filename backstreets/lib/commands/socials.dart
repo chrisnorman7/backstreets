2c4ffd787742d0faf35f3458b172563a4a1ab09f
Chris Norman <chris.norman2@googlemail.com>
2020-06-08 02:00:53 +0100

    Don't let players say nothing.

--- backstreets/lib/commands/socials.dart
+++ backstreets/lib/commands/socials.dart
@@ -10,6 +10,9 @@ import 'command_context.dart';
 /// The character wants to say something.
 Future<void> say(CommandContext ctx) async {
   final String text = ctx.args[0] as String;
+  if (text.isEmpty) {
+    return ctx.message('You say nothing. Good job!');
+  }
   final GameObject c = await ctx.getCharacter();
   c.doSocial(ctx.db, '%1N say%1s: "$text"', sound: socialSounds['say']);
 }
