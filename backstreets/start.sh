bbe4a42f578627f95fa3c2facc0005bf942dd5d6
Chris Norman <chris.norman2@googlemail.com>
2020-06-10 23:45:35 +0100

    Update start script to enable SSL by default.

--- backstreets/start.sh
+++ backstreets/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-clear && aqueduct serve -n 1 --no-color > errors.log 2>&1
+clear && aqueduct serve -n 1 --ssl-certificate-path ../../certs/fullchain.pem --ssl-key-path ../../certs/privkey.pem > errors.log 2>&1
