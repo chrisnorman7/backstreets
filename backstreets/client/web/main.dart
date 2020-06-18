27db67a0b1391a713e8337732481dc3dad4f9fb4
Chris Norman <chris.norman2@googlemail.com>
2020-06-18 11:14:48 +0100

    Stop blocking the page context menu.

--- backstreets/client/web/main.dart
+++ backstreets/client/web/main.dart
@@ -265,7 +265,7 @@ void main() {
       }
     }
   });
-  document.onContextMenu.listen((MouseEvent e) => e.preventDefault());
+  // document.onContextMenu.listen((MouseEvent e) => e.preventDefault());
   for (final Element e in querySelectorAll('.controls')) {
     e.onClick.listen((MouseEvent e) {
       e.stopPropagation();
