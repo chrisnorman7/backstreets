a196d22c6c166a98dbd1bff2f9363d49d960578c
Chris Norman <chris.norman2@googlemail.com>
2020-06-21 11:48:38 +0100

    Run aqueduct with no color.

--- backstreets/start-aqueduct.sh
+++ backstreets/start-aqueduct.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-clear && aqueduct serve -n 1 --ssl-certificate-path ../../certs/fullchain.pem --ssl-key-path ../../certs/privkey.pem > errors.log 2>&1
+clear && aqueduct serve --no-color -n 1 --ssl-certificate-path ../../certs/fullchain.pem --ssl-key-path ../../certs/privkey.pem > errors.log 2>&1
